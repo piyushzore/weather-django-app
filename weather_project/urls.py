ae82578cb46aa44e931e197af704403749d772eb
Piyush <147583597+piyushzore@users.noreply.github.com>
2025-12-10 18:05:04 +0530

    Update urls.py

--- weather_project/urls.py
+++ weather_project/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from core.views import home
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', home),                 
+    path('', TemplateView.as_view(template_name='demo.html')),  # ✅ ONE SINGLE DEMO PAGE
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
 ]
