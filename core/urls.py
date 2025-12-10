b6e8397f92b66b22d2a9529257e06a08d9d59d6d
Piyush <147583597+piyushzore@users.noreply.github.com>
2025-12-10 16:18:06 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,9 @@
+from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import CityViewSet, WeatherView, dashboard
-
-router = DefaultRouter()
-router.register(r'cities', CityViewSet, basename='city')
+from core.views import home
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('weather/<str:city>/', WeatherView.as_view(), name='weather-by-city'),
-    path('dashboard/', dashboard, name='dashboard'),
+    path('', home),   
+    path('admin/', admin.site.urls),
+    path('api/', include('core.urls')),
 ]
